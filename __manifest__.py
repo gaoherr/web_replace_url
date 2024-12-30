c37c00f6cf21deded75b2e8150e7901562aa5ec0
Your Name <you@example.com>
2024-12-30 13:01:04 +0800

    g

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2018 Simone Orsi - Camptocamp SA
 # License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
 {
-    "name": "Url odoo Replace",
+    "name": "Url odoo18 Replace",
     'version': '1.0.3',
     "author": "1311793927@qq.com",
     'support': '1311793927qq.com',
     "summary": "URL Replace  link",
     "description":"Odoo18 Replace odoo in hyperlink",
-    "category": "web",
+    "category": "Extra Tools",
     "license": "LGPL-3",
     "depends": ["web",'base'],
     "application": False,
